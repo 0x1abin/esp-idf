e55570b11098227e937bbbd87de8fe1095f845a5
watson8544 <854410664@qq.com>
2018-07-30 17:27:29 +0800

    change the macro of color

--- components/lcd/include/lcd_interface.h
+++ components/lcd/include/lcd_interface.h
@@ -22,25 +22,25 @@ extern "C" {
 #endif
 
 // Color definitions
-#define ILI9341_BLACK       0x0000      /*   0,   0,   0 */
-#define ILI9341_NAVY        0x000F      /*   0,   0, 128 */
-#define ILI9341_DARKGREEN   0x03E0      /*   0, 128,   0 */
-#define ILI9341_DARKCYAN    0x03EF      /*   0, 128, 128 */
-#define ILI9341_MAROON      0x7800      /* 128,   0,   0 */
-#define ILI9341_PURPLE      0x780F      /* 128,   0, 128 */
-#define ILI9341_OLIVE       0x7BE0      /* 128, 128,   0 */
-#define ILI9341_LIGHTGREY   0xC618      /* 192, 192, 192 */
-#define ILI9341_DARKGREY    0x7BEF      /* 128, 128, 128 */
-#define ILI9341_BLUE        0x001F      /*   0,   0, 255 */
-#define ILI9341_GREEN       0x07E0      /*   0, 255,   0 */
-#define ILI9341_CYAN        0x07FF      /*   0, 255, 255 */
-#define ILI9341_RED         0xF800      /* 255,   0,   0 */
-#define ILI9341_MAGENTA     0xF81F      /* 255,   0, 255 */
-#define ILI9341_YELLOW      0xFFE0      /* 255, 255,   0 */
-#define ILI9341_WHITE       0xFFFF      /* 255, 255, 255 */
-#define ILI9341_ORANGE      0xFD20      /* 255, 165,   0 */
-#define ILI9341_GREENYELLOW 0xAFE5      /* 173, 255,  47 */
-#define ILI9341_PINK        0xF81F
+#define ILI9341_BLACK       0      /*   0,   0,   0 */
+#define ILI9341_NAVY        128      /*   0,   0, 128 */
+#define ILI9341_DARKGREEN   32768      /*   0, 128,   0 */
+#define ILI9341_DARKCYAN    32896      /*   0, 128, 128 */
+#define ILI9341_MAROON      8388608      /* 128,   0,   0 */
+#define ILI9341_PURPLE      8388736      /* 128,   0, 128 */
+#define ILI9341_OLIVE       8421376      /* 128, 128,   0 */
+#define ILI9341_LIGHTGREY   12632256      /* 192, 192, 192 */
+#define ILI9341_DARKGREY    8421504      /* 128, 128, 128 */
+#define ILI9341_BLUE        255      /*   0,   0, 255 */
+#define ILI9341_GREEN       65280      /*   0, 255,   0 */
+#define ILI9341_CYAN        65535      /*   0, 255, 255 */
+#define ILI9341_RED         16515072      /* 255,   0,   0 */
+#define ILI9341_MAGENTA     16515327      /* 255,   0, 255 */
+#define ILI9341_YELLOW      16579584      /* 255, 255,   0 */
+#define ILI9341_WHITE       16579836      /* 255, 255, 255 */
+#define ILI9341_ORANGE      16557056      /* 255, 165,   0 */
+#define ILI9341_GREENYELLOW 11336748      /* 173, 255,  47 */
+#define ILI9341_PINK        16564426
 
 typedef void* lcd_handle_t;
 
